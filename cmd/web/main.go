d7d2d693b002c0b838fea36ede8881319689c473
Paweł Kromołowski <p.kromolowski@do-it.co>
2024-01-05 16:06:39 +0100

    init: from the end of chapter 5

---
+++ cmd/web/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"html/template"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"snippetbox.pawelkrml.com/internal/models"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type application struct {
+	logger        *slog.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
+}
+
+func main() {
+	addr := flag.String("addr", ":1337", "HTTP network address")
+
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "Database connection string")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	cache, err := newTemplateCache()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	app := &application{
+		logger:        logger,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: cache,
+	}
+
+	logger.Info("starting server", "port", *addr)
+
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module snippetbox.pawelkrml.com
+
+go 1.21.4
+
+require github.com/go-sql-driver/mysql v1.7.1 // indirect
